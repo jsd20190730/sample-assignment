30cdfa6deb43493a8ec4492e9835947279841f22
Kareem Grant <kareem@getuserwise.com>
2019-08-08 13:57:11 -0400

    initial commit

---
+++ assignment.js
@@ -0,0 +1,27 @@
+/***********
+Tags: strings
+
+Problem:
+
+Write a program that takes a name and prints out (using console.log()) the following welcome greeting: Hi {name}, thanks for signing up!
+
+Variables Required (feel free to add your own variables if needed):
+name (string)
+
+Test Cases:
+Use the following test cases to confirm your program meets the success criteria
+
+1. Given the following:
+    name = "Mark"
+
+   Result: Hi Mark, thanks for signing up!
+
+2. Given the following:
+    name = "Kiara"
+
+   Result: Hi Kiara, thanks for signing up!
+************/
+
+// Add your code below this line
+
+// Add your code above this line
